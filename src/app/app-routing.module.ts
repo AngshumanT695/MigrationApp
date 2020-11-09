f90da21c75390e6f63e9f4c127083c19945369c6
Angshuman <angshumang95@gmail.com>
2020-11-09 14:01:51 +0530

    UI Skeleton ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'firstPage', component: FirstPageComponent },
+  { path: 'secondPage', component: SecondPageComponent },
+  { path: '', redirectTo: '/firstPage', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
