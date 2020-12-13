fd40f33c16f00dbc4ffec0d3223823f0deb86e1f
Arnabi <arnabi98@hotmail.com>
2020-12-14 02:21:04 +0530

    reverting back

--- server-src/utils/perform-update/perform-update.ts
+++ server-src/utils/perform-update/perform-update.ts
@@ -4,7 +4,7 @@ import executeUpdate from '../utilities/execute-update';
 function performUpdate(updateRequest: UpdateRequest) {
   const updateOutput = executeUpdate(updateRequest, false, updateRequest.force);
   if (updateOutput.status !== 0){
-    throw new Error(updateOutput?.stderr||updateOutput?.error?.message);
+    throw new Error(`Update could not be performed.`);
   }
   return { message: `Angular update successful` };
 }
